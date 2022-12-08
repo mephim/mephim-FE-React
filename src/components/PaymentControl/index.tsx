6048015dbb99cafb7be7d9bd3ab8fe06e5c83963
lchau1@cmcglobal.vn <lchau1@cmcglobal.vn>
2022-12-08 17:31:16 +0700

    add timer cowndown

--- src/components/PaymentControl/index.tsx
+++ src/components/PaymentControl/index.tsx
@@ -1,16 +1,60 @@
-import './style.css';
-import React from "react";
-import {Col, Row} from "react-bootstrap";
-import { Select } from 'antd';
+import "./style.css";
+import React, { useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import { Select } from "antd";
+import ReactDOM from "react-dom";
+import Countdown from "react-countdown";
 
 function PaymentControl() {
-    return <div className="payment-control">
-        <Row>
-            <Col xs={12}>
-                <h4 className="text-center center mt-32 heading">PHƯƠNG THỨC THANH TOÁN</h4>
-            </Col>
-        </Row>
-    </div>;
+  const handleChange = (value: { value: string; label: React.ReactNode }) => {
+    console.log(value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
+  };
+  // Random component
+  const Completionist = () => <span>You are good to go!</span>;
+
+  // Renderer callback with condition
+  const renderer = ({ hours, minutes, seconds, completed }: any) => {
+    if (completed) {
+      // Render a complete state
+      return <Completionist />;
+    } else {
+      // Render a countdown
+      return (
+        <span>
+          {minutes}:{seconds}
+        </span>
+      );
+    }
+  };
+
+  return (
+    <div className="payment-control">
+      <Row>
+        <Col xs={12}>
+          <h4 className="text-center center mt-32 heading">
+            PHƯƠNG THỨC THANH TOÁN
+          </h4>
+
+          <Select
+            labelInValue
+            defaultValue={{ value: "lucy", label: "Lucy (101)" }}
+            style={{ width: 120 }}
+            onChange={handleChange}
+            options={[
+              {
+                value: "jack",
+                label: "Jack (100)",
+              },
+              {
+                value: "lucy",
+                label: "Lucy (101)",
+              },
+            ]}
+          />
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default PaymentControl;
